4946cdf18ee1ec204eefeada224213b1ca01f0fb
Sebastien35 <Sborgne@live.fr>
2024-11-28 16:47:18 +0100

    automatic invoice generation

--- ultradinapp/src/Controller/TestController.php
+++ ultradinapp/src/Controller/TestController.php
@@ -7,11 +7,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Dompdf\Dompdf;
 use Dompdf\Options;
+use App\Entity\Invoice;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class TestController extends AbstractController
 {
     #[Route('/test-pdf', name: 'app_test')]
-    public function tryPDF(): Response
+    public function tryPDF(UserRepository $userRepo, EntityManagerInterface $em): Response
     {
         try{
             $data = [
@@ -20,10 +23,18 @@ class TestController extends AbstractController
                     ['name' => 'Product 2', 'price' => 200],
                     ['name' => 'Product 3', 'price' => 300],
                 ],
-                'user' => ['email' => 'email@email.com'],
+                'user' => ['email' => 'user@email.com'],
                 'total' => 600,
             ];
             $filepath = CommonController::generateInvoicePdf($data);
+            $invoice = new Invoice();
+            $invoice->setFileUrl($filepath);
+            $invoice->setCreatedAt(new \DateTimeImmutable());
+            $user = $userRepo->findOneBy(['email' => $data['user']['email']]);
+            $invoice->setUser($user);
+            $em->persist($invoice);
+            $em->flush();
+
             return new Response('PDF generated successfully at: '.$filepath);
 
         } catch (\Exception $e) {
