7c14b34d1432b64c360ccc6a0a8e08743e8924b2
Sebastien35 <Sborgne@live.fr>
2024-11-28 23:27:10 +0100

    order save & invoice gen

---
+++ ultradinapp/config/Globals.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$company_name = "Ultradin";
+$company_email = "contact+ultradin.fr";
+$company_phone = "06 00 00 00 00";
+$company_address = "1 rue de la paix, 75000 Paris";
+$company_siret = "123 456 789 00001";
+$company_tva = "FR 00 000 000 000";
+$company_capital = "1000€";
+$company_rcs = "Paris 123 456 789";
+$company_ape = "1234Z";
+$company_legal = "SAS Ultradin";
+
+define('COMPANY_NAME', $company_name);
+define('COMPANY_EMAIL', $company_email);
+define('COMPANY_PHONE', $company_phone);
+define('COMPANY_ADDRESS', $company_address);
+define('COMPANY_SIRET', $company_siret);
+define('COMPANY_TVA', $company_tva);
+define('COMPANY_CAPITAL', $company_capital);
+define('COMPANY_RCS', $company_rcs);
+define('COMPANY_APE', $company_ape);
+define('COMPANY_LEGAL', $company_legal);
+
--- ultradinapp/src/Controller/TestController.php
+++ ultradinapp/src/Controller/TestController.php
@@ -8,8 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 use Dompdf\Dompdf;
 use Dompdf\Options;
 use App\Entity\Invoice;
+use App\Repository\ProductRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Order;
 
 class TestController extends AbstractController
 {
@@ -40,5 +42,54 @@ class TestController extends AbstractController
         } catch (\Exception $e) {
             return new Response('Error generating PDF: '.$e->getMessage());
         }
+    }
+
+    #[Route('/test-new-order', name: 'app_test_new_order')]
+    public function testOrder(ProductRepository $productRepo, EntityManagerInterface $em): Response{
+        try{
+        $order = new Order();
+        $order->setDateConfirmed(new \DateTimeImmutable());
+        $order->setOrderUuid(uniqid());
+        $order->setStatus('confirmed');
+        $order->setEta(new \DateTimeImmutable());
+        $order->setUser($this->getUser());
+        $order->addProduct($productRepo->find(1));
+        $order->addProduct($productRepo->find(2));
+        $order->addProduct($productRepo->find(3));
+        $price = 0;
+        foreach ($order->getProducts() as $product) {
+            $price += $product->getPrice();
+        }
+        $order->setTotalPrice($price);
+        
+        $em->persist($order);
+        $em->flush();
+        } catch (\Exception $e) {
+            return new Response('Error creating order: '.$e->getMessage());
+        }
+        try{
+        $data = [
+            'products' => array_map(function($product){
+                return ['name' => $product->getName(), 'price' => $product->getPrice()];
+            }, $order->getProducts()->toArray()),
+            'user' => ['email' => $order->getUser()->getEmail()],
+            'total' => $price,
+        ];
+    } catch (\Exception $e) {
+        return new Response('Error creating invoice data: '.$e->getMessage());
+    }
+    try{
+        $filepath = CommonController::generateInvoicePdf($data);
+        $invoice = new Invoice();
+        $invoice->setFileUrl($filepath);
+        $invoice->setCreatedAt(new \DateTimeImmutable());
+        $invoice->setUser($order->getUser());
+        $em->persist($invoice);
+        $em->flush();
+        return new Response('Order created successfully');  
+    } catch (\Exception $e) {
+        return new Response('Error generating PDF: '.$e->getMessage());
+    }
+        return new Response('Order created successfully');
     }
 }
